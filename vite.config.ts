42c3f36d2627f0cc020672a16840a8bc580065a2
kimddddh98 <amawang11@gmail.com>
2026-01-13 21:00:47 +0900

    chore: CORS 관련 proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(),react(), ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  console.log({VITE_API_BASE_URL : env.VITE_API_BASE_URL})
+  return {
+    plugins: [tailwindcss(),react(), ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
