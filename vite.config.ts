d2bd981ec5e39d60a1255d9b746653ae2ede98fb
kimddddh98 <amawang11@gmail.com>
2026-01-13 23:47:48 +0900

    misc: console 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
+import path from 'path'
 
 // https://vite.dev/config/
 
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
-  console.log({VITE_API_BASE_URL : env.VITE_API_BASE_URL})
   return {
-    plugins: [tailwindcss(),react(), ],
+    plugins: [tailwindcss(), react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
