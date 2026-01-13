9a8233cef8c6ca3f41226bcdfd6c25e17f82d7e2
kimddddh98 <amawang11@gmail.com>
2026-01-14 00:56:44 +0900

    chore: vite-plugin-svgr 플러그인 추가
    svg 아이콘 사용을 위해 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-
-// https://vite.dev/config/
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
-    plugins: [tailwindcss(), react()],
+    plugins: [tailwindcss(), svgr(), react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
