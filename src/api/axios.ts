e5fd112370ffde0794bcab920d8b0cddf00b879c
kimddddh98 <amawang11@gmail.com>
2026-01-16 02:55:53 +0900

    feat: 에러 reject 값 통일

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,9 @@
 import URL from '@/const/urls.const'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { getAccessToken, removeAccessToken } from './auth'
 import { ERROR_CODES } from '@/const/errors.const'
 import { ROUTES } from '@/routes/path'
+import { useToastStore } from '@/store/toastStore'
 
 export const http = axios.create({
   baseURL: URL.baseUrl,
@@ -27,19 +28,29 @@ http.interceptors.request.use(
 
 http.interceptors.response.use(
   (response) => response,
-  async (error) => {
+  async (error: AxiosError<BaseResponse<any>>) => {
     if (error) {
       // 401 에러 처리
-      if (error.response.data.code === ERROR_CODES.UNAUTHORIZED) {
+      if (!error.response) {
+        return Promise.reject<BaseResponse<any>>({
+          code: 'NETWORK_ERROR',
+          message: '네트워크 오류가 발생했습니다',
+        })
+      }
+
+      const { code } = error.response.data
+
+      if (code && code === ERROR_CODES.UNAUTHORIZED) {
         removeAccessToken()
         window.location.href = ROUTES.SIGN_IN
       }
-      console.log('에러 발생', error?.response?.data)
-      if (error?.response?.data) {
-        return Promise.reject(error?.response?.data)
-      }
+      const apiUrl = error.request.responseURL.split(
+        URL.webUrl + URL.baseUrl,
+      )[1]
+      console.log(`api 에러 발생 : ${apiUrl}`, error.response)
+
+      return Promise.reject<BaseResponse<any>>(error.response.data)
     }
-    return Promise.reject(error)
   },
 )
 
--- src/const/urls.const.ts
+++ src/const/urls.const.ts
@@ -1,6 +1,8 @@
 const API_BASE_URL = import.meta.env.VITE_API_PROXY_URL
+const WEB_BASE_URL = import.meta.env.VITE_WEB_BASE_URL
 export const URL = {
   baseUrl: API_BASE_URL,
-} as const;
+  webUrl: WEB_BASE_URL,
+} as const
 
-export default URL;
+export default URL
